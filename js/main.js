e832e28864ccfc5630af208647357f27268be531
jonathan odom <jonathan.odom87@gmail.com>
2012-11-19 15:13:04 -0600

    added all starting files

---
+++ js/main.js
@@ -0,0 +1,36 @@
+//Jonathan Odom
+//on track money v:0.1
+
+$('#home').on('pageinit',function(){
+	
+
+	//call for accordion view on home page
+	$(function() {
+        $( "#accordion" ).accordion({
+			active: false,
+			autoHeight: false,
+			navigation: true,
+			collapsible: true
+		});
+    });
+
+
+});
+
+//additem page functions
+$('#addItem').on('pageinit', function(){
+
+	
+});
+
+//display all data page functions
+$('#display').on('pageinit', function(){
+
+
+});
+
+// about page functions
+$('#about').on('pageinit', function(){
+
+
+});
